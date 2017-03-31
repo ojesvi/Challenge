c62d33f218214fb4d6108041a6267478563e2606
ojesvi <ojesvikushwah@gmail.com>
2017-03-31 15:14:31 +0200

    Update Startup.java

--- src/main/java/com/mediasaturn/challenge/Startup.java
+++ src/main/java/com/mediasaturn/challenge/Startup.java
@@ -1,11 +1,11 @@
-package com.mediasaturn.challenge;
+package com.opentext.challenge;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 /**
- * @author geroe
+ * @author Ojesvi
  * @since 16.01.2017
  */
 @SpringBootApplication
